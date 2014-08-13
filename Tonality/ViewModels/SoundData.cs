5271f7f11c6f4b80f4a0c6ee6bba40477b750a65
bucsox <cjf7911@live.com>
2014-08-13 18:47:14 -0400

    Initial

---
+++ Tonality/ViewModels/SoundData.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GalaSoft.MvvmLight;
+using System.Diagnostics;
+using Microsoft.Phone.Tasks;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+using System.Windows;
+using System.IO.IsolatedStorage;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Tonality.ViewModels
+{
+    public class SoundData : ViewModelBase, IEquatable<SoundData>
+    {
+        public string Title { get; set; }
+        public string FilePath { get; set; }
+        public string Items { get; set; }
+        public string Groups { get; set; }
+        #region New code
+        public string SavePath { get; set; }
+        public bool IsDownloaded
+        {
+            get
+            {
+                return IsolatedStorageFile.GetUserStoreForApplication().FileExists(this.SavePath) && this.Status == DownloadStatus.Downloaded;
+            }
+        }
+
+        private DownloadStatus status;
+        public DownloadStatus Status
+        {
+            get
+            {
+                return this.status;
+            }
+            set
+            {
+                if (this.status != value)
+                {
+                    this.status = value;
+                    this.RaisePropertyChanged(() => this.Status);
+                }
+            }
+        }
+
+        private int downloadProgress;
+        public int DownloadProgress
+        {
+            get
+            {
+                return this.downloadProgress;
+            }
+            set
+            {
+                if (this.downloadProgress != value)
+                {
+                    this.downloadProgress = value;
+                    this.RaisePropertyChanged(() => this.DownloadProgress);
+                }
+            }
+        }
+        #endregion
+        [XmlIgnore]
+        public RelayCommand<string> SaveSoundAsRingtone { get; set; }
+
+       
+        private void ExecuteSaveSoundAsRingtone(string soundPath)
+        {
+            if (IsDownloaded == false)
+            {
+                MessageBox.Show("Will not download until you short press atleast once to play sound");
+                return;
+            }
+            App.Current.RootVisual.Dispatcher.BeginInvoke(() =>
+            {
+
+                SaveRingtoneTask task = new SaveRingtoneTask();
+                task.Source = new Uri("isostore:/" + this.SavePath);
+                task.DisplayName = this.Title;
+                task.Show();
+            }
+               );
+        }
+          
+            
+        
+               
+        public SoundData()
+        {
+            SaveSoundAsRingtone = new RelayCommand<string>(ExecuteSaveSoundAsRingtone);
+            this.Status = DownloadStatus.NotDownloaded;
+            this.DownloadProgress = 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            SoundData other = obj as SoundData;
+
+            if (obj == null)
+            {
+                return false;
+            }
+
+            return this.Equals(other);
+        }
+
+        public bool Equals(SoundData other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.Title.Equals(other.Title);
+        }
+
+        public static bool operator ==(SoundData a, SoundData b)
+        {
+            if (object.ReferenceEquals(a, b)) return true;
+            if (object.ReferenceEquals(a, null)) return false;
+            if (object.ReferenceEquals(b, null)) return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(SoundData a, SoundData b)
+        {
+            if (object.ReferenceEquals(a, b)) return false;
+            if (object.ReferenceEquals(a, null)) return true;
+            if (object.ReferenceEquals(b, null)) return true;
+
+            return !a.Equals(b);
+        }
+
+    }
+}
---
+++ Tonality/ViewModels/SoundModel.cs
@@ -0,0 +1,138 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using Tonality.ViewModels;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace Tonality.ViewModels
+{
+    public class SoundModel : BindableBase
+    {
+        public SoundGroup Walter { get; set; }
+        public SoundGroup Jesse { get; set; }
+        public SoundGroup Saul { get; set; }
+        public SoundGroup Gus { get; set; }
+        public SoundGroup Jr { get; set; }
+        public SoundGroup Mike { get; set; }
+        public SoundGroup Sky { get; set; }
+        public SoundGroup Groups { get; set; }
+
+        public bool IsDataLoaded { get; set; }
+
+        public const string CustomSoundKey = "CustomSound";
+
+        public SoundModel()
+        {
+            this.LoadData();
+        }
+
+        public void LoadData()
+        {
+            Walter = LoadFromXml("Walter.xml");
+            Jesse = LoadFromXml("Jesse.xml");
+            Saul = LoadFromXml("Saul.xml");
+            Gus = LoadFromXml("Gus.xml");
+            Jr = LoadFromXml("Jr.xml");
+            Mike = LoadFromXml("Mike.xml");
+            Sky = LoadFromXml("Sky.xml");
+
+
+
+            IsDataLoaded = true;
+        }
+
+        private SoundGroup LoadFromXml(string xmlName)
+        {
+            SaveSoundGroup isolatedGroup = new SaveSoundGroup();
+            SaveSoundGroup assetsGroup;
+            XmlSerializer serializer = new XmlSerializer(typeof(SaveSoundGroup));
+
+            using (Stream fileStream = Application.GetResourceStream(new Uri("Xmls/" + xmlName, UriKind.Relative)).Stream)
+            {
+                assetsGroup = (SaveSoundGroup)serializer.Deserialize(fileStream);
+            }
+
+            if (IsolatedStorageFile.GetUserStoreForApplication().FileExists(xmlName))
+            {
+                using (Stream fileStream = IsolatedStorageFile.GetUserStoreForApplication().OpenFile(xmlName, FileMode.Open, FileAccess.Read))
+                {
+                    isolatedGroup = (SaveSoundGroup)serializer.Deserialize(fileStream);
+                }
+
+                foreach (var entry in assetsGroup.Items)
+                {
+                    if (!isolatedGroup.Items.Contains(entry))
+                    {
+                        isolatedGroup.Items.Add(entry);
+                    }
+                }
+                for (int i = 0; i < isolatedGroup.Items.Count; i++)
+                {
+                    if (!assetsGroup.Items.Contains(isolatedGroup.Items[i]))
+                    {
+                        isolatedGroup.Items.RemoveAt(i);
+                        i--;
+                    }
+                }
+
+            }
+            else
+            {
+                isolatedGroup = assetsGroup;
+            }
+
+            return new SoundGroup(isolatedGroup);
+        }
+
+        private void SaveToXml(SoundGroup group, string xmlName)
+        {
+            if (group != null)
+            {
+                Stream fileStream;
+                fileStream = IsolatedStorageFile.GetUserStoreForApplication().CreateFile(xmlName);
+
+                XmlSerializer serializer = new XmlSerializer(typeof(SaveSoundGroup));
+                serializer.Serialize(fileStream, new SaveSoundGroup(group));
+
+                fileStream.Close();
+                fileStream.Dispose();
+            }
+        }
+
+        public void SaveData()
+        {
+            this.SaveToXml(Walter, "Walter.xml");
+            this.SaveToXml(Jesse, "Jesse.xml");
+            this.SaveToXml(Saul, "Saul.xml");
+            this.SaveToXml(Gus, "Gus.xml");
+            this.SaveToXml(Jr, "Jr.xml");
+            this.SaveToXml(Mike, "Mike.xml");
+            this.SaveToXml(Sky, "Sky.xml");
+        }
+
+        private SoundGroup LoadCustomSounds()
+        {
+            SoundGroup data;
+            string dataFromAppSettings;
+
+            if (IsolatedStorageSettings.ApplicationSettings.TryGetValue(CustomSoundKey, out dataFromAppSettings))
+            {
+                data = JsonConvert.DeserializeObject<SoundGroup>(dataFromAppSettings);
+            }
+            else
+            {
+                data = new SoundGroup();
+                data.Title = "mine";
+            }
+
+            return data;
+        }
+    }
+}
+
